aeb603204fb99c2aa83c1df0738f9e5e19a81b1d
Anthony Galagade <agalagade@methys.com>
2018-04-26 11:35:29 +0200

    migrations fix

--- server/migrations/sqls/20180115101048-initial-up.sql
+++ server/migrations/sqls/20180115101048-initial-up.sql
@@ -4,14 +4,16 @@ CREATE TABLE users (
   username varchar(68) NOT NULL,
   password varchar(256) NOT NULL,
   name varchar(200) NOT NULL,
-  created_date varchar(68) NOT NULL
+  created_date varchar(68) NOT NULL,
+  PRIMARY KEY (id)
 );
 CREATE TABLE messages (
   id int(11) NOT NULL AUTO_INCREMENT,
   message varchar(256) NOT NULL,
   user_id int(11) NOT NULL,
-  unix_timestamp BIGINT(68) NOT NULL;
-  created_date varchar(68) NOT NULL
+  unix_timestamp BIGINT(68) NOT NULL,
+  created_date varchar(68) NOT NULL,
+  PRIMARY KEY (id)
 );
 
 
